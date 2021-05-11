e30357b597d7fbd22c790d9e24b16032264020f6
Артём Сегеда <mal4ik2k17@mail.ru>
2021-05-11 16:52:43 +0300

    commit for site2

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// var dot = document.getElementsByClassName("dot");
+
+
+// for (let index = 0; index < dot.length; index++) {
+//     dot[index].addEventListener("click", function() {
+//         alert(123);        
+//     });
+// }
+
+
+// dot[0].classList.toggle("active");
+// function slider(){
+//     var chey;
+//     for (let index = 0; index < dot.length; index++) {
+//         dot[index].classList.remove("active");
+//         if (this == dot[index]) {
+//             chey = index;
+//         }
+//     }
+
+//     this.classList.toggle("active");
+//     if (chey == 0) {
+//         document.getElementsByClassName("header").style.background = "url(img/slide1.png)"
+//     } else {
+//         if (chey == 1) {
+//             document.getElementsByClassName("header").style.background = "url(img/slide2.jpg)"
+//         } else {
+//             document.getElementsByClassName("header").style.background = "url(img/slide3.jpg)"
+//         }
+//     }
+// }
